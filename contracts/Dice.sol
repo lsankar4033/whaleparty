f70643ec817b21019c369cc6ed686fefcf626430
lsankar4033 <lsankar4033@gmail.com>
2018-04-27 14:36:00 -0700

    Add WIP Dice.sol

--- contracts/Dice.sol
+++ contracts/Dice.sol
@@ -3,11 +3,37 @@ pragma solidity ^0.4.4;
 import "./oraclize/oraclizeAPI.sol";
 
 // Phase 1: rolls a fair 'dice' (defined by min/max) for a fee (necessary b/c oraclize has a fee associated with it)
+// Phase 2: add betting + payouts
 contract Dice is usingOraclize {
+
+  // TODO: Perhaps add something to track if authenticity proof was invalid
+  struct GameData {
+    address player,
+    uint256 min,
+    uint256 max,
+    uint256 roll
+  }
+
+  mapping(bytes32 => GameData) _queryToGameData;
+  mapping(address => bytes32) _playerToLastQuery;
+
+  // TODO
+  event RollFinished(
+  );
 
   function Dice() {}
 
-  function roll(uint256 min, uint256 max) external returns (uint256) {
+  function roll(uint256 min, uint256 max) external {
+
+  }
+
+  // TODO: The method called by Oraclize
+  function __callback(bytes32 _queryId, string _result, bytes _proof) external {
+    // note, should verify that proof doesn't fail
+  }
+
+
+  function getLastRoll() external {
 
   }
 
