0ddc3d8b69d2cecf3a3a44de962e0adc37d2f70b
lsankar4033 <lsankar4033@gmail.com>
2018-04-27 17:21:54 -0700

    Fix typo in contract deployment script

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,6 @@
 var MockedDice = artifacts.require("./MockedDice.sol");
 
-module.exports = function(deployer) {
+module.exports = function(deployer, network) {
 
   // TODO: Do non-dev deploys
   if (network == "development") {
