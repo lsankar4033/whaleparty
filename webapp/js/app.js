0b336cc43091c614c2ae586012c9dd86f617a0ce
lsankar4033 <lsankar4033@gmail.com>
2018-05-03 20:32:22 -0700

    Add roll interaction to UI

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -1,15 +1,100 @@
+function ethToWei(eth) {
+  return Math.floor(eth * (10**18));
+}
+
+function weiToEth(wei) {
+  return wei / (10 ** 18);
+}
+
 App = {
   web3Provider: null,
   diceContract: null,
 
+  maxProfitWei: 0,
+
   init: async () => {
     await App.initContracts();
+    await App.initPage();
 
-    $('#test-roller').click( (e) => App.roll(1, 6) );
-    $('#get-last-roll').click( App.getLastRoll );
+    // TODO: Set up Eth event listeners
 
-    //
+    App.setupRollUI();
 
+    $("#roll-btn").on("click", App.roll);
+
+    $('#roll-again').click(function(){
+      $(".place-bet").fadeIn();
+      $(".Analysis").fadeIn();
+      $("#second-roll-under").fadeOut();
+      $(".fade2").fadeOut();
+      $(".result").fadeOut();
+    });
+  },
+
+  initContracts: async () => {
+    if (typeof web3 == 'undefined') {
+      alert("Couldn't find a web3 instance! Do you have metamask installed?");
+    } else {
+      App.web3Provider = web3.currentProvider;
+
+      let contractData = await $.getJSON('contracts/Dice.json');
+
+      let abstractContract = TruffleContract(contractData);
+      abstractContract.setProvider(App.web3Provider);
+
+      App.diceContract = await abstractContract.deployed();
+    }
+  },
+
+  initPage: async () => {
+    App.maxProfitWei = await App.diceContract.getMaxProfit();
+    let hasActiveRoll = await App.diceContract.hasActiveRoll();
+
+    if (!hasActiveRoll) {
+      // Display roll prompt
+      $('#roll-ongoing').fadeOut( () => $('#roll-ongoing').hide() );
+      $('#roll-prompt').show();
+    }
+  },
+
+  roll: async (min, max) => {
+    // Display roll ongoing
+    $('#roll-prompt').hide();
+    $('#roll-ongoing').show();
+    $('#loading').fadeIn();
+
+    // Submit roll to contract
+    let odds = parseInt($('#x').val());
+    let wager = ethToWei(parseFloat($('#wager').text()));
+    await App.diceContract.rollDice(odds, {value: wager});
+
+    // TODO: Add back in winning element when we get appropriate GameCompleted event
+    //$("#x2").text($('#x').val());
+    //$("#x2").css("color","black");
+    //$("#second-roll-under").fadeIn();
+    //$("#winningnumber").css("display","none");
+    //setTimeout(function(){
+      //$("#loading").css("display","none");
+      //var s = document.getElementById("winningnumber");
+      //s.value = $('#rangeInput').val()*1+0;
+      //$("#winningnumber").text($('#rangeInput').val()*1+0);
+
+      //if ($("#winningnumber").val() <= $('#rangeInput').val()) {
+        //$("#winningnumber").css("color","green");
+        //$("#win").css("display","block");
+
+      //} else {
+        //$("#winningnumber").css("color","red");
+        //$("#lose").css("display","block");
+      //}
+      //$(".fade2").fadeIn();
+      //// $(".place-bet").fadeIn();
+      //// $(".Analysis").fadeIn();
+    //}, 3000)
+  },
+
+  // Sets up roll UI for selecting odds + bet size
+  setupRollUI: () => {
     $('#rangeInput').on('input', function() {
       $("#x").text($('#rangeInput').val()*1+1);
       $("#chanceofwinning").text($('#rangeInput').val());
@@ -44,77 +129,10 @@ App = {
       $('.winningpot').text((.99*(1-$('#rangeInput').val()/100)*$('#betamount').val()/($('#rangeInput').val()/100)-$('#betamount').val()*.01).toFixed(8));
     });
 
-
     $('#rangeInput').on('input', function() {
       $('.winningpot').text((.99*(1-$('#rangeInput').val()/100)*$('#betamount').val()/($('#rangeInput').val()/100)-$('#betamount').val()*.01).toFixed(8)); //
     });
-
-    $("#test-roller").on("click", function(){
-      $(".place-bet").css("display","none");
-      $(".Analysis").css("display","none");
-      $("#loading").fadeIn()
-      $("#x2").text($('#x').val());
-      $("#x2").css("color","black");
-      $("#second-roll-under").fadeIn();
-      $("#winningnumber").css("display","none");
-      setTimeout(function(){
-      $("#loading").css("display","none");
-      var s = document.getElementById("winningnumber");
-      s.value = $('#rangeInput').val()*1+0;
-      $("#winningnumber").text($('#rangeInput').val()*1+0);
-
-      if ($("#winningnumber").val() <= $('#rangeInput').val()) {
-        $("#winningnumber").css("color","green");
-        $("#win").css("display","block");
-
-      } else {
-        $("#winningnumber").css("color","red");
-        $("#lose").css("display","block");
-      }
-      $(".fade2").fadeIn();
-      // $(".place-bet").fadeIn();
-      // $(".Analysis").fadeIn();
-    }, 3000)
-    });
-
-
-    $('#roll-again').click(function(){
-      $(".place-bet").fadeIn();
-      $(".Analysis").fadeIn();
-      $("#second-roll-under").fadeOut();
-      $(".fade2").fadeOut();
-      $(".result").fadeOut();
-    });
-  },
-
-  initContracts: async () => {
-    if (typeof web3 == 'undefined') {
-      alert("Couldn't find a web3 instance! Do you have metamask installed?");
-    } else {
-      App.web3Provider = web3.currentProvider;
-
-      let contractData = await $.getJSON('contracts/Dice.json');
-
-      console.log(`${JSON.stringify(contractData)}`);
-
-      let abstractContract = TruffleContract(contractData);
-      abstractContract.setProvider(App.web3Provider);
-
-      App.diceContract = await abstractContract.deployed();
-    }
-  },
-
-  roll: async (min, max) => {
-    // NOTE: This will eventually be changed to be the wager
-    let amountToPay = 0;
-    await App.diceContract.roll(min, max, {value: amountToPay});
-  },
-
-  getLastRoll: async () => {
-    let lastRoll = await App.diceContract.getLastRoll();
-    alert(`Your last roll was: ${lastRoll.toNumber()}`);
   }
-
 };
 
 $(function() {
