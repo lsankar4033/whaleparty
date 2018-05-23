ed62e0366a2f9cad8146cef66279d01382bf6391
lsankar4033 <lsankar4033@gmail.com>
2018-05-22 17:23:18 -0700

    Remove unused code

--- webapp/js/app.js
+++ webapp/js/app.js
@@ -267,39 +267,30 @@ App = {
     });
 
     // Get the modal
-        var modal = document.getElementById('myModal');
+    var modal = document.getElementById('myModal');
 
-        // Get the button that opens the modal
-        var btn = document.getElementById("btnsuccess");
+    // Get the button that opens the modal
+    var btn = document.getElementById("btnsuccess");
 
-        // Get the <span> element that closes the modal
-        var span = document.getElementsByClassName("close")[0];
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
 
-        // When the user clicks the button, open the modal
-        btn.onclick = function() {
-            modal.style.display = "block";
-        }
+    // When the user clicks the button, open the modal
+    btn.onclick = function() {
+      modal.style.display = "block";
+    }
 
-        // When the user clicks on <span> (x), close the modal
-        span.onclick = function() {
-            modal.style.display = "none";
-        }
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      modal.style.display = "none";
+    }
 
-        // When the user clicks anywhere outside of the modal, close it
-        window.onclick = function(event) {
-            if (event.target == modal) {
-                modal.style.display = "none";
-            }
-        }
-    // $('#rangeInput').on('input', function() {
-    //   var winning_amt = (.99*(1-$('#rangeInput').val()/100)*$('#betamount').val()/($('#rangeInput').val()/100)-$('#betamount').val()*.01);
-    //   var max_win_amt = weiToEth(App.maxProfitWei);
-    //   if (max_win_amt < winning_amt){
-    //     $('.winningpot').text((max_win_amt).toFixed(8));
-    //   } else{
-    //     $('.winningpot').text((winning_amt).toFixed(8));
-    //   }
-    // });
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target == modal) {
+        modal.style.display = "none";
+      }
+    }
   }
 };
 
